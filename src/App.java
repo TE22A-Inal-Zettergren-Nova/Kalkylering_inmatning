1466da74f262544b6368ce341451c9be7b5edacd
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-09-06 10:36:51 +0200

    inmatning med modulo

--- src/App.java
+++ src/App.java
@@ -2,25 +2,30 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Uppgift a");
-        Scanner inputA = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Ange tal 1: ");
-        int tal_ett = inputA.nextInt();
-
+        int tal_ett = input.nextInt();
         System.out.print("Ange tal 2: ");
-        int tal_två = inputA.nextInt();
-        
+        int tal_två = input.nextInt();
         System.out.println(tal_ett+"*"+tal_två+"="+(tal_ett*tal_två));
 
         System.out.println("Uppgift b");
-        Scanner inputB = new Scanner(System.in);
         System.out.print("Ange decimaltal 1: ");
-        double deci_ett = inputB.nextDouble();
-
+        double deci_ett = input.nextDouble();
         System.out.print("Ange decimaltal 2: ");
-        double deci_två = inputB.nextDouble();
+        double deci_två = input.nextDouble();
 
         System.out.print(deci_ett+"/"+deci_två+"="+(deci_ett/deci_två));
 
+        System.out.println("Uppgift c");
+        System.out.print("Ange tal 1: ");
+        int modulo_ett = input.nextInt();
+        System.out.print("Ange tal 2: ");
+        int modulo_två = input.nextInt();
 
+        System.out.println(modulo_ett+"%"+modulo_två+"="+(modulo_ett%modulo_två));
+
+
+        
     }
 }
