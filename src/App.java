8cdad7f8d3306d8862e8d7e9f2bd1926b167643c
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-09-06 10:14:23 +0200

    Multiplikation inmatning

--- src/App.java
+++ src/App.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
+        System.out.print("Ange tal 1: ");
+        int tal_ett = input.nextInt();
+
+        System.out.print("Ange tal 2: ");
+        int tal_två = input.nextInt();
         
+        System.out.println(tal_ett+"*"+tal_två+"="+(tal_ett*tal_två));
+
     }
 }
