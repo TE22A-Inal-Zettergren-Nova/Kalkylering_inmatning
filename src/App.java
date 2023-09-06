b9928d8973529807fadfae7ea2b9065e6ac7a88b
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-09-06 10:23:13 +0200

    Division med decimaltal

--- src/App.java
+++ src/App.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
+        System.out.println("Uppgift a");
+        Scanner inputA = new Scanner(System.in);
         System.out.print("Ange tal 1: ");
-        int tal_ett = input.nextInt();
+        int tal_ett = inputA.nextInt();
 
         System.out.print("Ange tal 2: ");
-        int tal_två = input.nextInt();
+        int tal_två = inputA.nextInt();
         
         System.out.println(tal_ett+"*"+tal_två+"="+(tal_ett*tal_två));
+
+        System.out.println("Uppgift b");
+        Scanner inputB = new Scanner(System.in);
+        System.out.print("Ange decimaltal 1: ");
+        double deci_ett = inputB.nextDouble();
+
+        System.out.print("Ange decimaltal 2: ");
+        double deci_två = inputB.nextDouble();
+
+        System.out.print(deci_ett+"/"+deci_två+"="+(deci_ett/deci_två));
+
 
     }
 }
